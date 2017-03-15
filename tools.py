7b0753728434ac1500e26b0d56a437827d046fbd
unknown <>
2017-03-15 21:41:07 +0800

    finish together matrix

--- tools.py
+++ tools.py
@@ -34,22 +34,47 @@ def calcIdf(IdfDict,wordDict):
 			IdfDict[word] = 1
 	return IdfDict
 
+#注意C[I,J]和C[J,I]
+def calcTogetherMatrix(togetherMatrix,wordDict): 
+	for word1 in wordDict:
+		word1 = word1.lower()
+		if word1 not in togetherMatrix:
+			togetherMatrix[word1] = {}
+		for word2 in wordDict:
+			word2 = word2.lower()
+			if word1 == word2:
+				continue
+			if word2 not in togetherMatrix[word1]:
+				togetherMatrix[word1][word2] = 0
+			else:
+				togetherMatrix[word1][word2] += 1
+	return togetherMatrix
+
+
 def buildTfIdfDict() :
 	IdfDict = {}
 	tfIdfDict = {}
+	togetherMatrix = {}
 	for i in range(0,300):
 		docLen,wordDictPerDoc = pretreat(str(i))
 		#print(str(i) + ":" + str(docLen))
+		calcTogetherMatrix(togetherMatrix,wordDictPerDoc)
 		IdfDict = calcIdf(IdfDict,wordDictPerDoc)
 		for word in wordDictPerDoc :
 			wordDictPerDoc[word] /= docLen
 		tfIdfDict[str(i)] = wordDictPerDoc
 
-	for docName in tfIdfDict:
-		for word in tfIdfDict[docName] :
-			tfIdfDict[docName][word] *= math.log(300/IdfDict[word],10)
+	# f = open('test1.txt', 'w')
+	# for item in togetherMatrix :
+	# 	for i in togetherMatrix[item]:
+	# 		f.write("[" + item + "]" + "[" + i + "]" + ":" + str(togetherMatrix[item][i]) + '\n')
+	# f.close()
 
-	return tfIdfDict
+	# for docName in tfIdfDict:
+	# 	for word in tfIdfDict[docName] :
+	# 		tfIdfDict[docName][word] *= math.log(300/IdfDict[word],10)
+
+	return tfIdfDict,togetherMatrix
 	# for docName in tfIdfDict:
 	# 	print(docName)
 	# 	print(tfIdfDict[docName])
@@ -67,7 +92,7 @@ def calcDistance(docToCalc,tfIdfDoc) :
 			dis += tfIdfDoc[word]
 	return dis
 
-def calcTop5(docToCalc,tfIdfDoc,docToCalcName):
+def calcTop5Doc(docToCalc,tfIdfDoc,docToCalcName):
 	l = []
 	for i in range(0,300):
 		if str(i) == docToCalcName:
@@ -77,7 +102,39 @@ def calcTop5(docToCalc,tfIdfDoc,docToCalcName):
 	Top5 = heapq.nlargest(5, l, key=lambda s: s[1])
 	print(Top5)
 
+def calcTop5Voc(target,togetherMatrix):
+	target = target.lower()
+	common = ["the","a","of","and","or",'was','in','at','to','by','on','s','from',
+				'his','as','an','is','','he','she','for','with','that','will','all','one','after',
+				'they','their','had','have','just','be','not','her','m','we','has','but','said',
+				'say','are','it']
+	if target not in togetherMatrix:
+		print(target + " not exist")
+		return
+	dict = sorted(togetherMatrix[target].items(), key=lambda d:d[1], reverse = True)
+	count = 0;
+	i = 0;
+	if target not in common:
+		while count < 5:
+			if dict[i][0].lower() not in common:
+				print(dict[i])
+				count += 1
+			i += 1
+	else:
+		n = 5
+		while i < n:
+			print(i)
+			if dict[i][0] == '':
+				n += 1
+				print("n=" + str(n))
+			else:
+				print(dict[i])
+			i += 1
+	#Top5 = heapq.nlargest(5, list(togetherMatrix[target]), key=lambda s: s[1])
+	#print(Top5)
+
 
 if __name__ == '__main__':
-	tfIdfDict = buildTfIdfDict()
-	calcTop5(tfIdfDict["0"],tfIdfDict,"0")
+	tfIdfDict,togetherMatrix = buildTfIdfDict()
+	calcTop5Voc("volleyball",togetherMatrix)
+		#calcTop5Doc(tfIdfDict[str(i)],tfIdfDict,str(i))
