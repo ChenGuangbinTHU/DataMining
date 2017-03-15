8c7011a1b72e28e00febfbdbf2e1737c81ef9308
unknown <>
2017-03-15 19:43:22 +0800

    finish calculate distance without ou

--- tools.py
+++ tools.py
@@ -2,6 +2,7 @@
 #coding = UFT-8
 import re
 import math
+import heapq
 
 def pretreat(documentName) :
 	result=[]
@@ -47,6 +48,8 @@ def buildTfIdfDict() :
 	for docName in tfIdfDict:
 		for word in tfIdfDict[docName] :
 			tfIdfDict[docName][word] *= math.log(300/IdfDict[word],10)
+
+	return tfIdfDict
 	# for docName in tfIdfDict:
 	# 	print(docName)
 	# 	print(tfIdfDict[docName])
@@ -57,5 +60,24 @@ def buildTfIdfDict() :
 	# 	f.write(item + ":" + str(IdfDict[item]) + '\n')
 	# f.close()
 
+def calcDistance(docToCalc,tfIdfDoc) :
+	dis = 0
+	for word in docToCalc:
+		if word in tfIdfDoc:
+			dis += tfIdfDoc[word]
+	return dis
+
+def calcTop5(docToCalc,tfIdfDoc,docToCalcName):
+	l = []
+	for i in range(0,300):
+		if str(i) == docToCalcName:
+			continue
+		dis = calcDistance(docToCalc,tfIdfDict[str(i)])
+		l.append([str(i),dis])
+	Top5 = heapq.nlargest(5, l, key=lambda s: s[1])
+	print(Top5)
+
+
 if __name__ == '__main__':
-	buildTfIdfDict()
+	tfIdfDict = buildTfIdfDict()
+	calcTop5(tfIdfDict["0"],tfIdfDict,"0")
